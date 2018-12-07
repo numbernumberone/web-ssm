deea827f055ed0dde5e34c5af34b62b24b6f54d7
franklin <frankin@github.com>
2018-12-07 23:02:02 +0800

    20181201

---
+++ LogTest/src/main/java/com/luban/Log4j.java
@@ -0,0 +1,14 @@
+package com.luban;
+
+import java.util.logging.Logger;
+
+public class Log4j {
+
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger("log4j");
+        logger.info("log4j");
+
+    }
+
+}
