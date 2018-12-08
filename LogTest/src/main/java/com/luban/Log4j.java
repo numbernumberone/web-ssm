23d80166d12dc7fc98cc51d457ca9c897b1e2839
franklin <frankin@github.com>
2018-12-08 22:50:09 +0800

    20181207 logtest

--- LogTest/src/main/java/com/luban/Log4j.java
+++ LogTest/src/main/java/com/luban/Log4j.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 public class Log4j {
 
 
-
+//-Dlog4j.configuration=file:////Users/wr/Desktop/python/web-ssm/LogTest/src/log4j.properties
 
     public static void log() {
         Logger logger = Logger.getLogger("log4j");
