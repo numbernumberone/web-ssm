f7c15c8535347373a5aef30af8a9325df96590d4
franklin <frankin@github.com>
2018-12-08 22:13:36 +0800

    20181207 logtest

---
+++ LogTest/src/main/java/com/luban/jcl.java
@@ -0,0 +1,12 @@
+package com.luban;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class jcl {
+    public static void main(String[] args) {
+        Log log  = LogFactory.getLog("jcl");
+                log.info("jcl");
+                //继承 或者实现接口
+    }
+}
