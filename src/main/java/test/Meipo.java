440194792d95a9c085c221f62f4af25724adf4c1
franklin <frankin@github.com>
2018-12-18 23:24:39 +0800

    shejimoshi detijiao

---
+++ src/main/java/test/Meipo.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class Meipo implements MethodInterceptor {
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        return null;
+    }
+}
